19a5f5e8eba23826411f201e82a17790a863d38f
Denitsa Yoncheva <denitsayoncheva@Denitsas-Air.home>
2020-03-26 10:34:11 +0100

    proba1

---
+++ main.py
@@ -0,0 +1,3 @@
+from petyk import *
+
+pet = Petyk13()
---
+++ petyk.py
@@ -0,0 +1,7 @@
+class Petyk13:
+
+	def __init__(self):
+		pass
+	def zdr(self):
+		self.name = input("Hi! What's your name?")
+		print("Welcome, ",self.name, "!")
